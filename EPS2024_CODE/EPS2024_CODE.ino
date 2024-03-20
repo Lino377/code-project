a51a56dd5d78273716cfb4ff19ebb0cfd44eb363
Lino377 <lino.brtrnd@gmail.com>
2024-03-20 11:03:25 +0100

    switch form u8g lib to u8g2 lib
    added a hourglass loading sceen

--- EPS2024_CODE/EPS2024_CODE.ino
+++ EPS2024_CODE/EPS2024_CODE.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>
-#include <U8glib.h>
-
+//#include <U8glib.h>
+#include <U8g2lib.h>
 
 #define ADC0 28
 #define ADC1 27
@@ -11,7 +11,8 @@
 #define SW2 19
 #define SCL 16
 #define SDA 17
-U8GLIB_SSD1309_128X64 u8g(U8G_I2C_OPT_NONE);  
+//U8GLIB_SSD1309_128X64 u8g(U8G_I2C_OPT_NONE);  
+U8G2_SSD1309_128X64_NONAME0_F_HW_I2C u8g2(U8G2_R0, /*reset=*/U8X8_PIN_NONE);  
 
 
 #define I2C_ADRESS 0x3C 
@@ -27,55 +28,49 @@ pinMode(SCL, OUTPUT);
 pinMode(SW1, INPUT_PULLUP);
 pinMode(SW2, INPUT_PULLUP);
 Wire.begin();
+u8g2.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  u8g.firstPage();
-  do {
-    u8g.undoRotation();
-    u8g.setFont(u8g_font_tpss);
-    u8g.drawStr(0,15,"Hello World!");
-  } while ( u8g.nextPage() );
-  delay(1000);
-  u8g.firstPage();
-  do{
-    u8g.setCursorFont(u8g_font_cursor);
-    u8g.setCursorStyle(82);
-    u8g.enableCursor();
-    u8g.setCursorPos(64,32);
-  }while ( u8g.nextPage() );
-  delay(250);
-  u8g.disableCursor();
-  u8g.firstPage();
-  do{
-    u8g.setRot90();
-    u8g.setCursorFont(u8g_font_cursor);
-    u8g.setCursorStyle(82);
-    u8g.enableCursor();
-    u8g.setCursorPos(32,64);
-  }while ( u8g.nextPage() );
-  delay(250);
-  u8g.disableCursor();
-  u8g.firstPage();
-  do{
-    u8g.setRot180();
-    u8g.setCursorFont(u8g_font_cursor);
-    u8g.setCursorStyle(82);
-    u8g.enableCursor();
-    u8g.setCursorPos(64,32);
-  }while ( u8g.nextPage() );
-  delay(250);
-  u8g.disableCursor();
-  u8g.firstPage();
-  do{
-    u8g.setRot270();
-    u8g.setCursorFont(u8g_font_cursor);
-    u8g.setCursorStyle(82);
-    u8g.enableCursor();
-    u8g.setCursorPos(32,64);
-  }while ( u8g.nextPage() );
-  u8g.disableCursor();
-  delay(250);
 
+  u8g2.firstPage();
+  //do{
+    //u8g2.setFont(u8g2_font_ncenB14_tr);
+    //u8g2.drawStr(0,15,"hello world!");
+    //u8g2.setFont(u8g2_font_streamline_all_t);
+    //u8g2.setFont(u8g2_font_streamline_interface_essential_loading_t);
+    //u8g2.setFontDirection(1);
+    //u8g2.drawGlyph(54, 38, 0x003D);
+  //} while( u8g2.nextPage() );
+  //delay(1000);
+
+    // put your main code here, to run repeatedly:
+  u8g2.firstPage();
+  do{
+    u8g2.setFont(u8g2_font_streamline_interface_essential_loading_t);
+    u8g2.setFontDirection(0);
+    u8g2.drawGlyph(54, 38, 0x003D);
+  }while ( u8g2.nextPage() );
+  delay(1000);
+  u8g2.firstPage();
+  do{
+    u8g2.setFont(u8g2_font_streamline_interface_essential_loading_t);
+    u8g2.setFontDirection(1);
+    u8g2.drawGlyph(54, 17, 0x003D);
+  }while ( u8g2.nextPage() );
+  delay(1000);
+  u8g2.firstPage();
+  do{
+    u8g2.setFont(u8g2_font_streamline_interface_essential_loading_t);
+    u8g2.setFontDirection(0);
+    u8g2.drawGlyph(54, 38, 0x003D);
+  }while ( u8g2.nextPage() );
+  delay(1000);
+  u8g2.firstPage();
+  do{
+    u8g2.setFont(u8g2_font_streamline_interface_essential_loading_t);
+    u8g2.setFontDirection(3);
+    u8g2.drawGlyph(75, 38, 0x003D);
+  }while ( u8g2.nextPage() );
+  delay(1000);
 }
