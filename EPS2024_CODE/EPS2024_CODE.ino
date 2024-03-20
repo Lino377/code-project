0dd1356d4869d8470b00a57febf7172bdadc72cd
Lino377 <lino.brtrnd@gmail.com>
2024-03-20 14:01:12 +0100

    menu proto
    adding menu

--- EPS2024_CODE/EPS2024_CODE.ino
+++ EPS2024_CODE/EPS2024_CODE.ino
@@ -1,5 +1,5 @@
 #include <Wire.h>
-//#include <U8glib.h>
+#include "main.h"
 #include <U8g2lib.h>
 
 #define ADC0 28
@@ -30,22 +30,130 @@ pinMode(SW2, INPUT_PULLUP);
 Wire.begin();
 u8g2.begin();
 }
+void guess(void){
+  u8g2.setFontDirection(0);
+  u8g2.firstPage();
+  do{
+    u8g2.setFont(u8g2_font_mozart_nbp_tr);
+    u8g2.drawStr(0,15,"wait");
+  } while( u8g2.nextPage() );
+  u8g2.firstPage();
+  delay(1000);
+  while(true){
+    u8g2.firstPage();
+    do{
+    u8g2.setFont(u8g2_font_mozart_nbp_tr);
+    u8g2.drawStr(0,15,"entre un nombre");
+    u8g2.drawStr(0,22," a deviner:");
+    u8g2.drawStr(5,35,"SW1 = 1");
+    u8g2.drawStr(5,55,"SW2 = 2");
+    if(digitalRead(SW1) == 0){
+      guess1();
+    }else if(digitalRead(SW2) == 0){
+      guess2();
+    }
+    //attachInterrupt(digitalPinToInterrupt(SW1), guess1, FALLING); 
+    //attachInterrupt(digitalPinToInterrupt(SW2), guess2, FALLING); 
+  } while( u8g2.nextPage() );
+  delay(1000);
+  }
+}
 
+void menu(void){
+
+}
 void loop() {
-
+  u8g2.setFontDirection(0);
   u8g2.firstPage();
-  //do{
-    //u8g2.setFont(u8g2_font_ncenB14_tr);
-    //u8g2.drawStr(0,15,"hello world!");
-    //u8g2.setFont(u8g2_font_streamline_all_t);
-    //u8g2.setFont(u8g2_font_streamline_interface_essential_loading_t);
-    //u8g2.setFontDirection(1);
-    //u8g2.drawGlyph(54, 38, 0x003D);
-  //} while( u8g2.nextPage() );
-  //delay(1000);
+  do{
+    delay(1000);
+    u8g2.setFont(u8g2_font_mozart_nbp_tr);
+    u8g2.drawStr(0,15,"appuiller sur SW1 SVP");
+    if(digitalRead(SW1) == 0){
+    guess();
+    }
+    //attachInterrupt(digitalPinToInterrupt(SW1), guess, FALLING); 
+    } while( u8g2.nextPage() );
+  delay(1000);
+}
 
-    // put your main code here, to run repeatedly:
+
+void guess1(void) {
+  // put your main code here, to run repeatedly:
+
+u8g2.firstPage();
+  do{
+    u8g2.setFont(u8g2_font_mozart_nbp_tr);
+    u8g2.setFontDirection(0);
+    u8g2.drawStr(0,15,"veuiller passienter");
+    u8g2.setFont(u8g2_font_battery19_tn);
+    u8g2.setFontDirection(1);
+    u8g2.drawGlyph(54, 38, 0x0030);
+  }while ( u8g2.nextPage() );
+  delay(1000);
   u8g2.firstPage();
+  do{
+    u8g2.setFont(u8g2_font_mozart_nbp_tr);
+    u8g2.setFontDirection(0);
+    u8g2.drawStr(0,15,"veuiller passienter.");
+    u8g2.setFont(u8g2_font_battery19_tn);
+    u8g2.setFontDirection(1);
+    u8g2.drawGlyph(54, 38, 0x0031);
+  }while ( u8g2.nextPage() );
+  delay(1000);
+  u8g2.firstPage();
+  do{
+    u8g2.setFont(u8g2_font_mozart_nbp_tr);
+    u8g2.setFontDirection(0);
+    u8g2.drawStr(0,15,"veuiller passienter..");
+    u8g2.setFont(u8g2_font_battery19_tn);
+    u8g2.setFontDirection(1);
+    u8g2.drawGlyph(54, 38, 0x0032);
+  }while ( u8g2.nextPage() );
+  delay(1000);
+  u8g2.firstPage();
+  do{
+    u8g2.setFont(u8g2_font_mozart_nbp_tr);
+    u8g2.setFontDirection(0);
+    u8g2.drawStr(0,15,"veuiller passienter...");
+    u8g2.setFont(u8g2_font_battery19_tn);
+    u8g2.setFontDirection(1);
+    u8g2.drawGlyph(54, 38, 0x0033);
+  }while ( u8g2.nextPage() );
+  delay(1000);
+  do{
+    u8g2.setFont(u8g2_font_mozart_nbp_tr);
+    u8g2.setFontDirection(0);
+    u8g2.drawStr(0,15,"veuiller passienter.");
+    u8g2.setFont(u8g2_font_battery19_tn);
+    u8g2.setFontDirection(1);
+    u8g2.drawGlyph(54, 38, 0x0034);
+  }while ( u8g2.nextPage() );
+  delay(1000);
+  u8g2.firstPage();
+    do{
+    u8g2.setFont(u8g2_font_mozart_nbp_tr);
+    u8g2.setFontDirection(0);
+    u8g2.drawStr(0,15,"veuiller passienter..");
+    u8g2.setFont(u8g2_font_battery19_tn);
+    u8g2.setFontDirection(1);
+    u8g2.drawGlyph(54, 38, 0x0035);
+  }while ( u8g2.nextPage() );
+  delay(1000);
+  u8g2.firstPage();
+  u8g2.setFontDirection(0);
+  u8g2.setFont(u8g2_font_mozart_nbp_tr);
+  u8g2.drawStr(0,15,"le nombre deviner est: ");
+  u8g2.setFont(u8g2_font_crox5t_tr);
+  u8g2.drawStr(5,35,"1");
+  u8g2.setFont(u8g2_font_mozart_nbp_tr);
+  delay(5000);
+}
+
+void guess2(void) {
+  // put your main code here, to run repeatedly:
+
+u8g2.firstPage();
   do{
     u8g2.setFont(u8g2_font_streamline_interface_essential_loading_t);
     u8g2.setFontDirection(0);
@@ -72,5 +180,11 @@ void loop() {
     u8g2.setFontDirection(3);
     u8g2.drawGlyph(75, 38, 0x003D);
   }while ( u8g2.nextPage() );
-  delay(1000);
+  u8g2.firstPage();
+  u8g2.setFontDirection(0);
+  u8g2.setFont(u8g2_font_mozart_nbp_tr);
+  u8g2.drawStr(0,15,"le nombre deviner est: ");
+  u8g2.setFont(u8g2_font_crox5t_tr);
+  u8g2.drawStr(5,35,"2");
+  delay(5000);
 }
